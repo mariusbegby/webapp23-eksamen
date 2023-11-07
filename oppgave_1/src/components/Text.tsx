8981b3384245261d55878d1e0b7de3fe36ba2e8d
Marius Begby <mariusbegby@gmail.com>
2023-11-07 16:06:12 +0100

    add props type

--- oppgave_1/src/components/Text.tsx
+++ oppgave_1/src/components/Text.tsx
@@ -1,4 +1,8 @@
-export default function TaskText(props: { text: string }) {
+type TaskTextProps = {
+  text: string
+}
+
+export default function TaskText(props: TaskTextProps) {
   const { text } = props
   return <p className="text-sm text-slate-400">{text}</p>
 }
