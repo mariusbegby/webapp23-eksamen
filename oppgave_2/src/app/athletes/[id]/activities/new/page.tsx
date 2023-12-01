1fc932f22afd2b001d6402e0b9b15bbaadb5f744
Marius Begby <mariusbegby@gmail.com>
2023-12-01 14:10:15 +0100

    improve design of creating activity

--- oppgave_2/src/app/athletes/[id]/activities/new/page.tsx
+++ oppgave_2/src/app/athletes/[id]/activities/new/page.tsx
@@ -120,56 +120,64 @@ export default function NewActivity() {
 
   return (
     <Page title="Opprett treningsøkt" backButtonLocation={`/athletes/${id}`}>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label className="block font-medium text-gray-700 dark:text-gray-200">
-            Dato:
-            <input
-              type="date"
-              name="date"
-              value={form.date}
-              onChange={handleChange}
-              className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
-            />
-          </label>
-        </div>
-        <div>
-          <label className="block font-medium text-gray-700 dark:text-gray-200">
-            Navn:
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-              className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
-            />
-          </label>
-        </div>
-        <div>
-          <label className="block font-medium text-gray-700 dark:text-gray-200">
-            Tags (separer med komma):
-            <input
-              type="text"
-              name="tags"
-              value={form.tags}
-              onChange={handleChange}
-              className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
-            />
-          </label>
-        </div>
-        <div>
-          <label className="block font-medium text-gray-700 dark:text-gray-200">
-            Type aktivitet:
-            <input
-              type="text"
-              name="type"
-              value={form.type}
-              onChange={handleChange}
-              className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
-            />
-          </label>
-        </div>
-        <div>
+      <form onSubmit={handleSubmit}>
+        <fieldset className="mb-8 space-y-4">
+          <legend className="text-lg font-medium text-gray-900 dark:text-gray-200">
+            Generell informasjon
+          </legend>
+          <div>
+            <label className="block font-medium text-gray-700 dark:text-gray-200">
+              Dato:
+              <input
+                type="date"
+                name="date"
+                value={form.date}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
+              />
+            </label>
+          </div>
+          <div>
+            <label className="block font-medium text-gray-700 dark:text-gray-200">
+              Navn:
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
+              />
+            </label>
+          </div>
+          <div>
+            <label className="block font-medium text-gray-700 dark:text-gray-200">
+              Tags (separer med komma):
+              <input
+                type="text"
+                name="tags"
+                value={form.tags}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
+              />
+            </label>
+          </div>
+          <div>
+            <label className="block font-medium text-gray-700 dark:text-gray-200">
+              Type aktivitet:
+              <input
+                type="text"
+                name="type"
+                value={form.type}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
+              />
+            </label>
+          </div>
+        </fieldset>
+        <fieldset className="mb-8 space-y-4">
+          <legend className="text-lg font-medium text-gray-900 dark:text-gray-200">
+            Spørsmål
+          </legend>
           <label className="block font-medium text-gray-700 dark:text-gray-200">
             Antall spørsmål:
             <input
@@ -180,24 +188,26 @@ export default function NewActivity() {
               className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
             />
           </label>
-        </div>
-        {form.questions.map((question, index) => (
-          <div key={index}>
-            <label className="block font-medium text-gray-700 dark:text-gray-200">
-              Spørsmål {index + 1}:
-              <input
-                type="text"
-                value={question}
-                onChange={(e) => {
-                  handleChange(e, index)
-                }}
-                className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
-              />
-            </label>
-          </div>
-        ))}
-
-        <div>
+          {form.questions.map((question, index) => (
+            <div key={index}>
+              <label className="block font-medium text-gray-700 dark:text-gray-200">
+                Spørsmål {index + 1}:
+                <input
+                  type="text"
+                  value={question}
+                  onChange={(e) => {
+                    handleChange(e, index)
+                  }}
+                  className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
+                />
+              </label>
+            </div>
+          ))}
+        </fieldset>
+        <fieldset className="space-y-4">
+          <legend className="text-lg font-medium text-gray-900 dark:text-gray-200">
+            Intervaller
+          </legend>
           <label className="block font-medium text-gray-700 dark:text-gray-200">
             Antall intervaller:
             <input
@@ -208,37 +218,40 @@ export default function NewActivity() {
               className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
             />
           </label>
-        </div>
-
-        {form.intervals.map((interval, index) => (
-          <div key={index}>
-            <label className="block font-medium text-gray-700 dark:text-gray-200">
-              Intervall {index + 1} Varighet:
-              <input
-                type="text"
-                value={interval.duration}
-                onChange={(e) => {
-                  handleChange(e, index, true)
-                }}
-                className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
-              />
-            </label>
-            <label className="mt-4 block font-medium text-gray-700 dark:text-gray-200">
-              Intervall {index + 1} Intensitet:
-              <input
-                type="text"
-                value={interval.intensity}
-                onChange={(e) => {
-                  handleChange(e, index, true, true)
-                }}
-                className="mt- block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
-              />
-            </label>
-          </div>
-        ))}
+          {form.intervals.map((interval, index) => (
+            <div key={index} className="flex space-x-4">
+              <div className="flex-1">
+                <label className="block font-medium text-gray-700 dark:text-gray-200">
+                  Intervall {index + 1} Varighet:
+                  <input
+                    type="text"
+                    value={interval.duration}
+                    onChange={(e) => {
+                      handleChange(e, index, true)
+                    }}
+                    className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
+                  />
+                </label>
+              </div>
+              <div className="flex-1">
+                <label className="block font-medium text-gray-700 dark:text-gray-200">
+                  Intervall {index + 1} Intensitet:
+                  <input
+                    type="text"
+                    value={interval.intensity}
+                    onChange={(e) => {
+                      handleChange(e, index, true, true)
+                    }}
+                    className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 sm:text-sm"
+                  />
+                </label>
+              </div>
+            </div>
+          ))}
+        </fieldset>
         <button
           type="submit"
-          className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          className="mt-4 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
         >
           Opprett økt
         </button>
